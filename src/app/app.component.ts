0997329f46e93b17d0e3521d2a8e5e008274f972
kiwokuhle <39264305+kiwokuhle@users.noreply.github.com>
2020-03-09 10:09:35 +0200

    item list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import * as firebase from 'firebase';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Router } from '@angular/router';
+import { Router,NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  showHead: boolean = false;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -18,9 +19,27 @@ export class AppComponent {
     private routes: Router
   ) {
     this.initializeApp();
-    this.getAuth();
+    // this.getAuth();
   }
+//   ionViewWillEnter(){
+//     this.initializeApp();
+// this.getAuth(); 
+//  // on route change to '/login', set the variable showHead to false
+//  this.routes.events.forEach((event) => {
+//   if (event instanceof NavigationStart) {
+//     if (event['url'] == '/login') {
+//       this.showHead = false;
+//     } else if ( event['url'] == '/pro' || event['url'] == '/details'||
+//     event['url'] == '/categorylist' || event['url'] == '/aobut-us' || event['url'] == '/quries' ||
+//     event['url'] == '/spacials' 
 
+//     ){
+//       // console.log("NU")
+//       this.showHead = true;
+//     }
+//   }
+// });
+// }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
