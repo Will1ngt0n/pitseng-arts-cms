20e5466aa7f4f72f044d815f1e4f41dc96239c56
449sharon <50619633+449sharon@users.noreply.github.com>
2020-03-10 17:14:41 +0200

    updates

--- src/app/pages/queries/queries.page.ts
+++ src/app/pages/queries/queries.page.ts
@@ -28,6 +28,14 @@ uid
   subject
   constructor( public toastCtrl: ToastController
   ) {
+  
+   }
+
+  ngOnInit() {
+   
+
+    console.log("ssssss ", firebase.auth().currentUser.email);
+    
     if(firebase.auth().currentUser) {
       this.admin.email = firebase.auth().currentUser.email;
       this.uid = firebase.auth().currentUser.uid;
@@ -35,20 +43,18 @@ uid
        console.log('error user not logged in');
        
      }
-   }
 
-  ngOnInit() {
-   
+
     this.getMessage()
     setTimeout(() => {
-      this.showList(0, this.message[0]);
+      // this.showList(0, this.message[0]);
       console.log('index', this.userMessage )
     }, 1000);
   }
  
   getMessage(){
    
-    this.db.collection('Messages').get().then(snapshot => {
+    this.db.collection('ContactUs').get().then(snapshot => {
       console.log('messges', this.message);
       
       if( this.message = []){
--- src/app/pages/users-orders/users-orders.page.ts
+++ src/app/pages/users-orders/users-orders.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-users-orders',
@@ -7,79 +8,31 @@ import * as firebase from 'firebase';
   styleUrls: ['./users-orders.page.scss'],
 })
 export class UsersOrdersPage implements OnInit {
-  dbOrder = firebase.firestore().collection('Order');
-  dbUser = firebase.firestore().collection("UserProfile");
-  dbHistory = firebase.firestore().collection('orderHistory');
-  ordersPlaced = [];
-  orderHistory = [];
-  myProfile = [];
-  Allorders = [];
-  users = []
-  myProduct = false;
+  pendingOrders : Array<any> = []
+  orderHistory : Array<any> = []
 
-  constructor() { }
+  constructor(private productsService : ProductsService,
+    public modalController: ModalController) { }
 
   ngOnInit() {
-    this.GetOrders();
-    this.viewDetails();
-    this.getOrderHistory()
-    let obj = { name: '', uid: '' };
-    this.dbUser.onSnapshot(data => {
-      data.forEach(item => {
-        this.myProfile.push({data:item.data().name, id : item.id});
-        obj.name = item.data().email;
-        obj.uid = item.data().uid
-        this.users.push(obj);
-        obj = { name: '', uid: '' };
-        // console.log("users ",  this.users);
-      })
+  }
+  getPendingOrders(){
+    return this.productsService.getPendingOrders().then( res => {
+      console.log(res);
+      this.pendingOrders = res
     })
   }
-  viewDetails() {
-   
-    this.dbOrder.onSnapshot(data => {
-      this.ordersPlaced = [];
-    data.forEach(item => {
-        
-      })
-   
-
-
-    })
-
-
-  }
-  userProfiles() {
-    this.ordersPlaced.forEach((i) => {
-
+  getClosedOrders(){
+    return this.productsService.getClosedOrders().then( res => {
+      console.log(res);
+      this.orderHistory = res
     })
   }
-  GetOrders() {
-
-    this.dbOrder.onSnapshot((data) => {
-      // console.log("olx", data);
-      if (this.Allorders = []) {
-        this.myProduct = true
-        data.forEach((item) => {
-          this.Allorders.push({ ref: item.id, info: item.data(), total: item.data() })
-        })
-        // console.log("ccc", this.Allorders);
-        // 
-      } else {
-        this.myProduct = false
-      }
-
-    })
+  dismiss() {
+    this.modalController.dismiss({
+      'dismissed': true
+    });
   }
-  getOrderHistory() {
-    this.dbHistory.onSnapshot((res) => {
-      this.orderHistory = [];
-      res.forEach((doc) => {
-        this.dbUser.doc(doc.data().userID).onSnapshot((item)=>{
-          this.orderHistory.push({info:doc.data(), user: item.data().name, ref: doc.id});
-        })
-      })
-    })
-  }
+
 
 }
