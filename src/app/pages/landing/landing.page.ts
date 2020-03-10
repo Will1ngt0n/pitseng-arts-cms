3de7debe2a9c0e5b625635e98b2613f43a61a11c
Zanele Refiloe nkosi <50693705+ZaneleNkosi@users.noreply.github.com>
2020-03-10 17:16:41 +0200

    changes

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -20,58 +20,58 @@ export class LandingPage implements OnInit {
   }
 
   ngOnInit() {
-    this.getSales()
-    this.getProducts()
-    this.getPendingOrders()
-    this.getClosedOrders()
+    // this.getSales()
+    // this.getProducts()
+    // this.getPendingOrders()
+    // this.getClosedOrders()
   }
-  navigateToItemsList(){
+  // navigateToItemsList(){
      
-  }
-  getProducts(){
-    return this.productsService.getProducts().then( res => {
-      console.log(res);
-      this.inventory = res
-      let order = this.inventory.sort((a,b) => {
-        let c = a.data.timesOrdered
-        let d = b.data.timesOrdered
-        return d - c
-      });
-      order.splice(6)
-      this.bestSellers = order
-      console.log(order);
-      console.log(this.bestSellers);
+  // }
+  // getProducts(){
+  //   return this.productsService.getProducts().then( res => {
+  //     console.log(res);
+  //     this.inventory = res
+  //     let order = this.inventory.sort((a,b) => {
+  //       let c = a.data.timesOrdered
+  //       let d = b.data.timesOrdered
+  //       return d - c
+  //     });
+  //     order.splice(6)
+  //     this.bestSellers = order
+  //     console.log(order);
+  //     console.log(this.bestSellers);
       
-    })
-  }
-  getSales(){
-    return this.productsService.getSales().then( res => {
-      console.log(res);
-      this.sales = res
-      this.maxPercentage = Math.max(...this.sales.map(o=>o['data'].percentage), this.sales[0]['data'].percentage);
-      console.log(this.maxPercentage);
+  //   })
+  // }
+  // getSales(){
+  //   return this.productsService.getSales().then( res => {
+  //     console.log(res);
+  //     this.sales = res
+  //     this.maxPercentage = Math.max(...this.sales.map(o=>o['data'].percentage), this.sales[0]['data'].percentage);
+  //     console.log(this.maxPercentage);
       
-    })
-  }
-  getPendingOrders(){
-    return this.productsService.getPendingOrders().then( res => {
-      console.log(res);
-      this.pendingOrders = res
-    })
-  }
-  getClosedOrders(){
-    return this.productsService.getClosedOrders().then( res => {
-      console.log(res);
-      this.orderHistory = res
-    })
-  }
-  viewItems(item){
-    console.log(item);
-    this.router.navigate(['items-list', item])
-  }
-  viewMore(para){
-    if(para === 'specials'){
-      this.router.navigate(['specials'])
-    }
-  }
+  //   })
+  // }
+  // getPendingOrders(){
+  //   return this.productsService.getPendingOrders().then( res => {
+  //     console.log(res);
+  //     this.pendingOrders = res
+  //   })
+  // }
+  // getClosedOrders(){
+  //   return this.productsService.getClosedOrders().then( res => {
+  //     console.log(res);
+  //     this.orderHistory = res
+  //   })
+  // }
+  // viewItems(item){
+  //   console.log(item);
+  //   this.router.navigate(['items-list', item])
+  // }
+  // viewMore(para){
+  //   if(para === 'specials'){
+  //     this.router.navigate(['specials'])
+  //   }
+  // }
 }
