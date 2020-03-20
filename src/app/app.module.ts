0cab94a993a847e9cc11b635ffd9970f32543fab
449sharon <50619633+449sharon@users.noreply.github.com>
2020-03-20 16:56:27 +0200

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,17 +18,27 @@ import { OrderDetailsPageModule } from './pages/order-details/order-details.modu
 import { OrdersListPageModule } from './pages/orders-list/orders-list.module';
 import { FaqsPage } from './pages/faqs/faqs.page';
 import { FaqsPageModule } from './pages/faqs/faqs.module';
-
 var firebaseConfig = {
-  apiKey: "AIzaSyD96pHc4rjR85yGc7y1Es4e1mGocFqHVjE",
-  authDomain: "pitseng-arts.firebaseapp.com",
-  databaseURL: "https://pitseng-arts.firebaseio.com",
-  projectId: "pitseng-arts",
-  storageBucket: "pitseng-arts.appspot.com",
-  messagingSenderId: "628652538196",
-  appId: "1:628652538196:web:b17d822fe6eb6702f9f0c1",
-  measurementId: "G-N7X15WH9QB"
+  apiKey: "AIzaSyCnRo3S9hOi1R9vMpnYNW5ajcpJh5G1tvo",
+  authDomain: "pitsengtest.firebaseapp.com",
+  databaseURL: "https://pitsengtest.firebaseio.com",
+  projectId: "pitsengtest",
+  storageBucket: "pitsengtest.appspot.com",
+  messagingSenderId: "728167140242",
+  appId: "1:728167140242:web:7b16d2f988fee781c90f4b",
+  measurementId: "G-TKFFCKLRTQ"
 };
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyD96pHc4rjR85yGc7y1Es4e1mGocFqHVjE",
+//   authDomain: "pitseng-arts.firebaseapp.com",
+//   databaseURL: "https://pitseng-arts.firebaseio.com",
+//   projectId: "pitseng-arts",
+//   storageBucket: "pitseng-arts.appspot.com",
+//   messagingSenderId: "628652538196",
+//   appId: "1:628652538196:web:b17d822fe6eb6702f9f0c1",
+//   measurementId: "G-N7X15WH9QB"
+// };
 // var firebaseConfig = {
 //   apiKey: "AIzaSyAxqR5pSWPudGtn3cvZlubPjwY-U6K5NRc",
 //   authDomain: "pitsengdb-e8f33.firebaseapp.com",
